8f41f116984f3caf088b0761b38c9ac997c5df3f
Edwin Contreras <ed25gonz@gmail.com>
2024-08-01 21:16:29 -0400

    chore: Add Tailwind CSS configuration files and dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/css/tailwind.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
