1e176d155e74391ff4c9343def7d03e543ed30d5
Edwin Contreras <ed25gonz@gmail.com>
2024-08-05 20:51:52 -0400

    feat: Add @tailwindcss/typography dependency and update Tailwind CSS configuration
    This commit adds the "@tailwindcss/typography" dependency to the devDependencies in the package.json file. It also updates the tailwind.config.js file to include the new dependency in the plugins array. Additionally, the colors object in the theme section of the tailwind.config.js file is extended to include three new color options: primary, secondary, and tertiary.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // purge: [], tailwind ya no usa PurgeCSS
+  // darkMode: false, // or 'media' or 'class'
   content: ["./public/index.html", "./src/css/tailwind.css"],
   theme: {
+    extend: {
+      colors: {
+        primary: '#CC2D4A',
+        secondary: '#8FA206',
+        tertiary: '#61AEC9',
+      }
+    },
+  },
+
+  variants: {
     extend: {},
   },
-  plugins: [],
+
+  corePlugins: {
+    aspectRatio: false,
+  },
+
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
 
